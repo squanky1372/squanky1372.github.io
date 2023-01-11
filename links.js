d8e5604f4adf4a5a8c8a80916b4af21f4240683c
felipejannarone <116711040+squanky1372@users.noreply.github.com>
2023-01-11 10:39:52 -0600

    controller
    Basic controller tester.
    Beginning of multiplayer driving.

---
+++ links.js
@@ -0,0 +1,14 @@
+var links_database = [
+    ["./html/twilight-head.html", "/html/img/Boxes3.png"], //Body waving w twilight's head
+    ["./html/images.html", "/html/img/Boxes2.png"], //Rotating images of cats
+    ["./planets/Planets.html","/html/img/Boxes4.png"], //Final graphics solar system project
+    ["/Trains/index.html", "/html/img/Boxes1.png"], //CTA Trains project
+    ["/weather/weather_daily.html", "/html/img/Boxes5.png"], //Animated weather project
+    ["/weather/weather_basic.html", "/html/img/Boxes6.png"], //simplified 15-day forecast
+    ["/html/data-processing/p5letters.html", "/html/img/Boxes7.png"], //wordle word visualizer
+    ["/html/data-processing/p5letters input.html", "/html/img/Boxes8.png"], //5-letter word constraints app
+    ["/controller/controller-p5basics.html", "/html/img/Boxes12.png"], //controller tester
+    ["/controller/multicanvas.html", "/html/img/Boxes11.png"], //multicanvas racing game
+    
+    // ["index.html", "/html/img/Boxes9.png"] //this page
+]
