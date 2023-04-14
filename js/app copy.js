1b328e303a66391f67dab39fbda7e7b5990cfda2
felipejannarone <116711040+squanky1372@users.noreply.github.com>
2023-04-13 21:06:10 -0500

    Update app copy.js

--- js/app copy.js
+++ js/app copy.js
@@ -1,6 +1,6 @@
 const http = require("http")
 const fs = require('fs')
-const port = 3000
+const port = 8888
 
 const server = http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type': 'text/html'})
