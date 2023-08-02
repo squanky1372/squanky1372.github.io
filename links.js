6ab959580895626299c601cd45b333058fa1d041
felipejannarone <116711040+squanky1372@users.noreply.github.com>
2023-08-01 22:58:15 -0400

    Update links.js
    - moved the cubes around
    - removed solar system, it's broken
    - removed test weather program

--- links.js
+++ links.js
@@ -1,21 +1,22 @@
 var links_database = [
-    ["./html/twilight-head.html", "/html/img/Boxes3.png"], //Body waving w twilight's head
-//    ["./html/images.html", "/html/img/Boxes2.png"], //Rotating images of cats
-    ["./planets/Planets.html","/html/img/Boxes4.png"], //Final graphics solar system project
-    ["/Trains/index.html", "/html/img/Boxes1.png"], //CTA Trains project
+    ["/music/music finder.html", "/html/img/Boxes13.png"], //Music chords finder tool
     ["/weather/weather_daily.html", "/html/img/Boxes5.png"], //Animated weather project
-    ["/weather/weather_basic.html", "/html/img/Boxes6.png"], //simplified 15-day forecast
+    ["/weather/weather_wheel_input.html", "/html/img/Boxes15.png"], //weather wheel with inputs 
+    ["/controller/controller-p5basics.html", "/html/img/Boxes12.png"], //controller tester
+    ["/Trains/index.html", "/html/img/Boxes1.png"], //CTA Trains project
     ["/html/data-processing/p5letters.html", "/html/img/Boxes7.png"], //wordle word visualizer
     ["/html/data-processing/p5letters input.html", "/html/img/Boxes8.png"], //5-letter word constraints app
-    ["/controller/controller-p5basics.html", "/html/img/Boxes12.png"], //controller tester
+    ["/html/data-processing/p5letters.html", "/html/img/Boxes7.png"], //Mini-Finder
     ["/controller/multicanvas.html", "/html/img/Boxes11.png"], //multicanvas racing game
-    ["/music/music finder.html", "/html/img/Boxes13.png"],
-    ["/stereogram/dual-stereogram.html", "/html/img/Boxes14.png"],
-    ["/weather/weather_wheel_input.html", "/html/img/Boxes15.png"],
-    ["Unity/sabodash/index.html", "/html/img/Boxes16.png"],
-    ["Unity/pool/index.html", "/html/img/Boxes17.png"],
-    ["https://bit.ly/3NlbFah", "/html/img/Boxes18.png"],
-    ["/html/data-processing/p5letters.html", "/html/img/Boxes7.png"]
+    ["/stereogram/dual-stereogram.html", "/html/img/Boxes14.png"], //Dual stereogram test program
+    ["Unity/sabodash/index.html", "/html/img/Boxes16.png"], //Sabodash game
+    ["Unity/pool/index.html", "/html/img/Boxes17.png"], //Pool game
+    ["https://bit.ly/3NlbFah", "/html/img/Boxes18.png"], //Link to AWS server
+    ["./html/twilight-head.html", "/html/img/Boxes3.png"], //Body waving w twilight's head
     
+    
+    // ["/weather/weather_basic.html", "/html/img/Boxes6.png"], //simplified 15-day forecast
+    // ["./html/images.html", "/html/img/Boxes2.png"], //Rotating images of cats
+    // ["./planets/Planets.html","/html/img/Boxes4.png"], //Final graphics solar system project (broken)  
     // ["index.html", "/html/img/Boxes9.png"] //this page
 ]
