8e88f4f752e6add41e35df7e13fef6cc8eff3312
felipejannarone <116711040+squanky1372@users.noreply.github.com>
2024-03-02 17:45:40 -0500

    New Car Link
    Retired multicanvas racing game, replacing it instead with the server-based time trials car game

--- links.js
+++ links.js
@@ -8,20 +8,20 @@ var links_database = [
     ["/2024/n-letter-bars-2024.html", "/html/img/Boxes7.png"], //wordle word visualizer
     ["/html/data-processing/p5letters input.html", "/html/img/Boxes8.png"], //5-letter word constraints app
     ["/html/data-processing/minifinder.html", "/html/img/Boxes26.png"], //Mini-Finder
-    ["/controller/multicanvas.html", "/html/img/Boxes11.png"], //multicanvas racing game
     ["/html/dual-stereogram.html", "/html/img/Boxes14.png"], //Dual stereogram test program
     ["Unity/sabodash/index.html", "/html/img/Boxes16.png"], //Sabodash game
     ["Unity/pool/index.html", "/html/img/Boxes17.png"], //Pool game
+    ["./html/twilight-head.html", "/html/img/Boxes3.png"], //Body waving w twilight's head
     ["http://squanky.net:3000/", "/html/img/Boxes22.png"], //Raycaster
     ["http://squanky.net:3002/", "/html/img/Boxes23.png"], //3D house
     ["http://squanky.net:3001/", "/html/img/Boxes19.png"], //MorseMate
     //??? 3D-Earth
     ["http://squanky.net:3003/", "/html/img/Boxes20.png"], //Buzzer server
-    ["./html/twilight-head.html", "/html/img/Boxes3.png"], //Body waving w twilight's head
     ["http://squanky.net:3005", "/html/img/Boxes21.png"], //Boston trains program
+    ["http://squanky.net:3007/", "/html/img/Boxes11.png"], //racing game
     ["/homepage/index.html", "/html/img/Boxes24.png"], //Homepage   
     
-    
+    // ["/controller/multicanvas.html", "/html/img/Boxes11.png"], //multicanvas racing game
     // ["/weather/weather_basic.html", "/html/img/Boxes6.png"], //simplified 15-day forecast
     // ["./html/images.html", "/html/img/Boxes2.png"], //Rotating images of cats
     // ["./planets/Planets.html","/html/img/Boxes4.png"], //Final graphics solar system project (broken)  
