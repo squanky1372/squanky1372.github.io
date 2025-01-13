c6e47884db84d3a0654445af6acf00695751ab85
felipejannarone <116711040+squanky1372@users.noreply.github.com>
2025-01-12 23:28:01 -0500

    Speech/GPT update

---
+++ 2024/speech/speech_repeat/script_input.js
@@ -0,0 +1,36 @@
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+var SpeechGrammarList = SpeechGrammarList || window.webkitSpeechGrammarList
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+
+var recognition = new SpeechRecognition();
+recognition.continuous = false;
+recognition.lang = 'en-US';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+
+var diagnostic = document.querySelector('.output');
+var bg = document.querySelector('html');
+
+document.getElementById("record").onclick = function ()  {
+  recognition.start();
+  console.log('Ready to receive a color command.');
+}
+
+recognition.onresult = function(event) { // See "script original backup" for desc of event struct
+  var spoken_words = event.results[0][0].transcript;
+  diagnostic.textContent = 'Result received: ' + spoken_words + '.';
+  console.log('Confidence: ' + event.results[0][0].confidence);
+  speak(spoken_words)
+}
+
+recognition.onspeechend = function() {
+  recognition.stop();
+}
+
+recognition.onnomatch = function(event) {
+  diagnostic.textContent = "I didn't recognise that color.";
+}
+
+recognition.onerror = function(event) {
+  diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
+}
