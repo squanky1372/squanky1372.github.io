5108dbd8422ba678e08f3f8e08a0683695c18eed
felipejannarone <116711040+squanky1372@users.noreply.github.com>
2025-03-17 12:48:51 -0400

    blacklist

--- homepage/functions/words.js
+++ homepage/functions/words.js
@@ -41,4 +41,28 @@ function whitelist(terminal, letters){
     }
 }
 
-export{scrabble, whitelist}
+function blacklist(terminal, letters){
+    for(var i = 2; i < 16; i++){
+        var words = fetch(`./data/${i}-letter-words.json`)
+        .then(response => response.json())
+        .then(text => { 
+            var wordlist = []
+            text.forEach(word_i => {wordlist.push(word_i.word)})
+            console.log(wordlist)
+            var validWords = []
+            wordlist.forEach((word, i) => {
+                var isValid = true;
+                Array.from(word).forEach((letter,i) => {if(letters.includes(letter))isValid =  false;})
+                if(isValid) validWords.push(word)
+            })
+            if(validWords.length > 0){
+                terminal.echo("" + validWords[0].length + ":" + validWords)
+                terminal.echo()
+
+            }
+            // return wordlist; 
+        })
+    }
+}
+
+export{scrabble, whitelist, blacklist}
