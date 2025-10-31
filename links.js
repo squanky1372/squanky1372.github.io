599d8d5c73f3dfbfefcb5caf211fc5ccba2632ad
felipejannarone <116711040+squanky1372@users.noreply.github.com>
2025-10-31 17:58:03 -0400

    Update links.js

--- links.js
+++ links.js
@@ -41,6 +41,7 @@ var links_database = [
     ["/2025/games_dev/blokus.html", "/html/img/Boxes49.png"], //Blokus
     ["/2025/games/sliding_blocks.html", "/html/img/Boxes50.png"], //Blokus
     ["/2025/games/daily_mino.html", "/html/img/Boxes51.png"], //Daily Mino puzzle
+    ["/2025/games/squong.html", "/html/img/Boxes10.png"], //Squong
 
     // ["http://squanky.net:3001/", "/html/img/Boxes19.png"], //MorseMate
     // ["/controller/multicanvas.html", "/html/img/Boxes11.png"], //multicanvas racing game
